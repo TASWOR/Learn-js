a3bbec9621e70386cb95375593e885788dd14072
TASWOR <you@example.com>
2017-12-03 20:14:02 +0200

    Update homeWork lesson2 JavaScript Advanced

---
+++ Itvdn(JavaScript Advanced)/002_Работа с документами/hw/4.js
@@ -0,0 +1,29 @@
+﻿var get={};
+get.class=function () {
+    var a = document.getElementsByClassName("class");
+    for(var i = 0;i<a.length;i++){
+        a[i].style.color="red";
+    }
+};
+get.class2=function () {
+    var a=document.getElementsByClassName("class2");
+    for(var i=0;i<a.length;i++){
+        a[i].style.color="blue";
+    }
+};
+get.borderClass = function () {
+    var a = document.getElementsByClassName("class");
+    for(var i=0;i<a.length;i++){
+        a[i].style.border="1px solid red";
+    }
+};
+get.borderClass2=function () {
+    var a =document.getElementsByClassName("class2");
+    for(var i=0;i<a.length;i++){
+        a[i].style.border="1px solid blue";
+    }
+};
+get.all=function () {
+    var a = document.getElementById("all");
+    a.style.border="1px solid yellow";
+}
